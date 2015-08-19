fa902e956eb84b1ccd430805aa3c1907e1716430
davidfdriscoll <david.f.driscoll@gmail.com>
2015-08-19 11:00:33 -0700

    tweak to corner popup code

--- production/html/lyric_mapping.js
+++ production/html/lyric_mapping.js
@@ -1,12 +1,10 @@
 $(document).mouseup(function (e)
   {
       var container = $("#corner_popup");
-      console.log("click registered in mouseup");
-      console.log(container);
   
       if (!container.is(e.target) // if the target of the click isn't the container...
           && container.has(e.target).length === 0 // ... nor a descendant of the container
-          && !$("#essayBox").is(':visible'))
+          && !$("#essayBox").is(':visible')) // and if the orbis popup is already gone, or was never there in the first place
       {
           container.hide();
       }
