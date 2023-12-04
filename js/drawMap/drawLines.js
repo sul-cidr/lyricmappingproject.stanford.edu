278135a966153e0af80558dbf5fed25ad7a78369
David F. Driscoll <david.f.driscoll@gmail.com>
2023-12-04 17:07:14 -0500

    add transparent line

--- js/drawMap/drawLines.js
+++ js/drawMap/drawLines.js
@@ -10,16 +10,38 @@ export function drawLines(map, calculatedLines) {
     if (line.dotted) {
       dashArray = "8, 8";
     }
-    const geodesic = L.geodesic([fromLatLng, toLatLng], { color: line.color, weight: line.weight, dashArray: dashArray });
-    if (line.popupHtml)
-      geodesic.bindPopup(line.popupHtml);
-    geodesic.bindTooltip(line.name);
+
+    const geodesic = L.geodesic(
+      [fromLatLng, toLatLng],
+      {
+        color: line.color,
+        weight: line.weight,
+        dashArray: dashArray
+      }
+    );
     const decorator = L.polylineDecorator(geodesic, {
       patterns: [
         { offset: 10, repeat: 200, symbol: L.Symbol.arrowHead({ pathOptions: { color: line.color, fillOpacity: 1, weight: 0 } }) }
       ]
     });
+    const transparentLine = L.geodesic(
+      [fromLatLng, toLatLng],
+      {
+        opacity: 0,
+        weight: line.weight * 20
+      }
+    );
+
+    if (line.popupHtml) {
+      geodesic.bindPopup(line.popupHtml);
+      transparentLine.bindPopup(line.popupHtml);
+    }
+
+    geodesic.bindTooltip(line.name);
+    transparentLine.bindTooltip(line.name);
+
     map.currentLayerGroup.addLayer(geodesic);
     map.currentLayerGroup.addLayer(decorator);
+    map.currentLayerGroup.addLayer(transparentLine);
   }
 }
